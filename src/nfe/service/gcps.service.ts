6ac5c42d8536366d10166deaca7f6d3724229656
userautoval-bit <userautoval@gmail.com>
2026-04-13 14:53:30 -0300

    buscar no completo ou parte do nome

--- src/nfe/service/gcps.service.ts
+++ src/nfe/service/gcps.service.ts
@@ -1,40 +1,44 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Gcps } from "../model/gcps.entity";
-import { Repository } from "typeorm";
-import { HttpException, HttpStatus } from "@nestjs/common";
+import { ILike, Repository } from "typeorm";
+import { HttpException, HttpStatus, NotFoundException } from "@nestjs/common";
 
-export class GcpsService{
+export class GcpsService {
 
      constructor(
-        @InjectRepository(Gcps)
-        private gcpsRepository: Repository<Gcps>,
-    ){}
+          @InjectRepository(Gcps)
+          private gcpsRepository: Repository<Gcps>,
+     ) { }
 
-      async findAll(): Promise<Gcps[]> {
-         return await this.gcpsRepository.find();
+     async findAll(): Promise<Gcps[]> {
+          return await this.gcpsRepository.find();
      }
 
 
 
-//METODOS ESPECIAIS 
+     //METODOS ESPECIAIS 
 
      // Método para buscar um GCP pelo número da nota fiscal (nf)
      async findByNf(nf: string): Promise<Gcps[]> {
           const registro = await this.gcpsRepository.find({ where: { nf } });
 
-          if(!registro || registro.length === 0) {
+          if (!registro || registro.length === 0) {
                throw new HttpException('Registro não encontrado', HttpStatus.NOT_FOUND);
           }
-          
+
           return registro;
      }
 
      //Método para buscar pelo nome do cliente
      async findByCliente(cliente: string): Promise<Gcps[]> {
-          const registros = await this.gcpsRepository.find({ where: { cliente } });
+          const registros = await this.gcpsRepository.find({
+               where: {
+                    cliente: ILike(`%${cliente}%`) // O ILike faz a mágica da busca parcial
+               }
+          });
 
-          if(!registros || registros.length === 0) {
-               throw new HttpException('Registro não encontrado', HttpStatus.NOT_FOUND);
+          if (registros.length === 0) {
+               throw new NotFoundException(`Nenhum cliente encontrado com o termo: ${cliente}`);
           }
 
           return registros;
